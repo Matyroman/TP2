d6497b19d6a8ec5a095384dd1be5a24262868dce
Matytute <165533228+Matyroman@users.noreply.github.com>
2025-06-12 20:27:07 -0300

    Create config.php
    Signed-off-by: Matytute <165533228+Matyroman@users.noreply.github.com>

---
+++ config.php
@@ -0,0 +1,28 @@
+<?php
+
+//start session on web page
+session_start();
+
+//config.php
+
+//Include Google Client Library for PHP autoload file
+require_once 'vendor/autoload.php';
+
+//Make object of Google API Client for call Google API
+$google_client = new Google_Client();
+
+//Set the OAuth 2.0 Client ID | Copiar "Aqui tu ID DE CLIENTE"
+$google_client->setClientId('113200675580-2kav2aluqirvm4r1vf4cqvprds0u52am.apps.googleusercontent');
+
+//Set the OAuth 2.0 Client Secret key Aqui tu CLAVE
+$google_client->setClientSecret('CSPX-5M0VM2zFJweasupB_OsFl9lhd9s1');
+
+//Set the OAuth 2.0 Redirect URI | URL AUTORIZADO
+$google_client->setRedirectUri('http://localhost/TP2/google-login/index.php');
+
+// to get the email and profile 
+$google_client->addScope('email');
+
+$google_client->addScope('profile');
+
+?>
